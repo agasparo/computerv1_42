ef9416392f282f2aaf3b3dcca318defa4fae4bd5
arthur <arthurysiite@gmail.Com>
2020-03-01 16:10:45 +0100

    update test

--- src/test/test.go
+++ src/test/test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"bytes"
+	"strings"
+	"github.com/fatih/color"
 )
 
 type Testes struct {
@@ -20,7 +22,21 @@ func main() {
 
 	AllTests := make([]Testes, 1, 100)
 	AddTest(AllTests, "syntax error 1 (double egale)", "/Users/arthur/Desktop/computorv1/main.go", "2 * X^0 == 1 * X^1", "Notice", "regex")
+	Header()
 	Run(AllTests)
+}
+
+func Header() {
+
+	fmt.Println("\n\n")
+	color.Magenta(" ::::::::   ::::::::  ::::    ::::  :::::::::  :::    ::: ::::::::::: :::::::::: :::::::::       :::     :::   :::   ")
+	color.Magenta(":+:    :+: :+:    :+: +:+:+: :+:+:+ :+:    :+: :+:    :+:     :+:     :+:        :+:    :+:      :+:     :+: :+:+:   ")
+	color.Magenta("+:+        +:+    +:+ +:+ +:+:+ +:+ +:+    +:+ +:+    +:+     +:+     +:+        +:+    +:+      +:+     +:+   +:+   ")
+	color.Magenta("+#+        +#+    +:+ +#+  +:+  +#+ +#++:++#+  +#+    +:+     +#+     +#++:++#   +#++:++#:       +#+     +:+   +#+   ")
+	color.Magenta("+#+        +#+    +#+ +#+       +#+ +#+        +#+    +#+     +#+     +#+        +#+    +#+       +#+   +#+    +#+   ")
+	color.Magenta("#+#    #+# #+#    #+# #+#       #+# #+#        #+#    #+#     #+#     #+#        #+#    #+#        #+#+#+#     #+#   ")
+	color.Magenta(" ########   ########  ###       ### ###         ########      ###     ########## ###    ###          ###     ####### ")
+	color.Magenta("\n\nMain test by : Agasparo\n\n")
 }
 
 func AddTest(table []Testes, name string, filename string, Commande string, Outpout string, Type string) {
@@ -48,10 +64,23 @@ func Run(table []Testes) {
 		    log.Fatal(err)
 		}
 		fmt.Printf("test [%s] : ", table[i].Name)
-		CheckRes(outb.String(), table[i].Outpout, table[i].Type_search)
+		if CheckRes(outb.String(), table[i].Outpout, table[i].Type_search) {
+			color.Set(color.FgGreen)
+			fmt.Println(" [ OK ]")
+			color.Unset()
+		} else {
+			color.Set(color.FgRed)
+			fmt.Println(" [ KO ]")
+			color.Unset()
+		}
 	}
 }
 
-func CheckRes(Outpout string, attOuput string, Type string) {
+func CheckRes(Outpout string, attOuput string, Type string) (bool) {
 
+	re := strings.Index(Outpout, attOuput)
+	if re != -1 {
+		return (true)
+	}
+	return (false)
 }

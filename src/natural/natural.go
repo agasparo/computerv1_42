b5900c98109eeff154837c0317c345b248938abf
arthur <arthurysiite@gmail.Com>
2020-03-07 13:33:38 +0100

    update natural converter

--- src/natural/natural.go
+++ src/natural/natural.go
@@ -9,29 +9,47 @@ func Convert(str string) (string) {
 
 	tab := strings.Split(str, " ")
 	new_str := ""
+	var in = 0
 
 	for i := 0; i < len(tab); i++ {
 
-		if i % 4 == 0 {
+		in = 0
+		new_str += tab[i]
+		new_str += " *"
 
-			new_str += tab[i]
-			new_str += " *"
-
-			if len(tab[i + 2]) < 2 {
-				new_str += " X^1"
+		if len(tab[i + 2]) < 3 {
+			new_str += " X^"
+			new_str += powerof(tab[i + 2])
+			if tab[i + 1] == "*" {
+				in = 3
 			} else {
-				new_str += " "
-				new_str += tab[i + 2]
+				in = 1
 			}
-
-			if i + 3 < len(tab) {
-				new_str += " "
-				new_str += tab[i + 3]
-				new_str += " "
-			}
-
-			fmt.Println(new_str)
+		} else {
+			new_str += " "
+			new_str += tab[i + 2]
 		}
+
+		if i + 3 < len(tab) {
+			new_str += " "
+			new_str += "/"//tab[i + 3]
+			new_str += " "
+		}
+
+		if in > 0 {
+			i += in
+		} else {
+			i += 3
+		}
+		fmt.Println(new_str)
 	}
 	return (new_str)
+}
+
+func powerof(str string) (string) {
+
+	if  str[0] != 'X' {
+		return ("0")
+	}
+	return ("1")
 }

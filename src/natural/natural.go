d830c63ca9c43103ebbd9c6de21da04794d52e61
arthur <arthurysiite@gmail.Com>
2020-03-07 13:39:16 +0100

    update natural converter

--- src/natural/natural.go
+++ src/natural/natural.go
@@ -9,17 +9,18 @@ func Convert(str string) (string) {
 
 	tab := strings.Split(str, " ")
 	new_str := ""
-	var in = 0
+	var in, x = 0, 0
 
 	for i := 0; i < len(tab); i++ {
 
 		in = 0
-		new_str += intorx(tab[i])
+		x = 0
+		new_str += intorx(tab[i], &x)
 		new_str += " *"
 
 		if len(tab[i + 2]) < 3 {
 			new_str += " X^"
-			new_str += powerof(tab[i + 2])
+			new_str += powerof(tab[i + 2], x)
 			if tab[i + 1] == "*" {
 				in = 3
 			} else {
@@ -46,16 +47,20 @@ func Convert(str string) (string) {
 	return (new_str)
 }
 
-func intorx(str string) (string) {
+func intorx(str string, x *int) (string) {
 
 	if str == "X" {
+		*x = 1
 		return ("1")
 	}
 	return (str)
 } 
 
-func powerof(str string) (string) {
+func powerof(str string, x int) (string) {
 
+	if x == 1 {
+		return ("1")
+	}
 	if  str[0] != 'X' {
 		return ("0")
 	}
